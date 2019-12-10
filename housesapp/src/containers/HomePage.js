ef96e75cd17b7ef86b87fe4672f35c406e6cab9c
DanielR <danielalejandror89@gmail.com>
2019-12-09 19:08:47 -0500

    building navbar

---
+++ housesapp/src/containers/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Navbar from '/Users/Drakarui/Documents/ProyectoInmobiliaria/ProyectoInmobiliaria/housesapp/src/components/NavBar.js'
+class HomePage extends Component {
+
+    render(){
+        return(
+            <Navbar/>
+
+        )
+    }
+
+}
+
+export default HomePage;
