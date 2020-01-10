6c22bf0f396c0af0950d91478c26384eba8ee649
iTheia <martinez.1ded@gmail.com>
2020-01-10 12:43:38 -0600

    que carajos paso mierda

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Login from './Components/LoginRegister/Login'
 import Register from './Components/LoginRegister/Register'
 import proveShit from './Components/Proves/proveShit.js'
 import ImmovablePage from './Containers/Immovable'
-import axios from 'axios'
-import SeederImmovable from './Schemas/SeederImmovable'
 
 import './App.css'
 
@@ -26,22 +24,6 @@ export default function App() {
   const updateSearchingProps = (propsObject) => {
     return setSearchingProps(propsObject)
   }
-
-  //DESCOMENTAR PARA ANADIR DATOS RANDOM A LA BASE DE DATOS
-  //PREVIAMENTE INICIAR mongo y mongod EN DOS CMD PARA INICIAR LA BASE DE DATOS
-/*   const preferences = ["Amoblado", "Zona para niños", "Gimnasio", "Cancha(s) de Squash", "Acceso para discapacitados"
-    , "Piscina", "Zonas verdes", "Cancha(s) de tennis", "Cancha(s) de fútbol", "Cancha(s) de Basket", "Verificado"
-    , "Cerca Transporte Público", "Cerca a Centros Comerciales", "Cerca a Parques", "Cerca a Supermercados", "Cerca a colegios / universidades"
-    , "Zona de BBQ", "Ascensor", "Con Balcón", "Con Vigilancia", "En conjunto cerrado", "Cuarto de servicio", "Con chimenea"
-    , "Con deposito", "Con zona de lavandería", "Se permiten mascotas", "Se permite fumar"]
-    
-  useEffect(() => {
-    const randomImmovable = SeederImmovable(40, ["apartamento", "casa", "lote"], preferences,["Compra nuevo", "Compra usado", "Compra nuevo y usado","Arriendo"],["lugar 1", "lugar 2", "lugar 3"])
-    randomImmovable.forEach(immovable => {
-      axios.post('/api/immovable', immovable)
-        .then(res => { console.log(res) })
-    });  
-}, []) */
 
   return (
     <Fragment>
--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -1,33 +1,12 @@
-import React, { useState, Fragment} from 'react'
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import React, { useState, Fragment, useEffect} from 'react'
 import PrincipalSearch from '../Components/PrincipalSearch/PrincipalSearch'
 
-import ImmovableComp from '../Containers/immovableComp'
 
-export default function HomePage() {
+export default function HomePage({ updateSearchingProps }) {
     return (
-        <>
-            <Router>
-                <Nav></Nav>
-                <Switch>
-                    <Route path="/login" render={()=>(
-                       <Login/>   
-                    )}></Route>
-                    <Route path="/register" render={()=>(
-                       <Register/>   
-                    )}></Route>
-                    <Route path="/inmuebles" render={()=>(
-                       <ImmovableComp/>   
-                    )}></Route>
-                    <Fragment>
-            <div>
-                <PrincipalSearch updateSearchingProps={updateSearchingProps}></PrincipalSearch>
-            </div>
-        </Fragment>    
-                </Switch>
-            </Router>
-            
-        </>
+        <div>
+            <PrincipalSearch updateSearchingProps={updateSearchingProps}></PrincipalSearch>
+        </div>
     )
 
 }
